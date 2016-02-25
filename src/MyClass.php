6a5b06a8040e5f409cd661ad355085bebdcb1d16
Stacey Vetzal <stacey@vetzal.com>
2016-02-25 14:22:17 -0500

    give folks something a little interesting to look at, give refactoring hint in test

--- src/MyClass.php
+++ src/MyClass.php
@@ -1,4 +1,9 @@
 <?php
 
 class MyClass {
+
+    public function adder($arg1, $arg2) {
+        return $arg1 + $arg2;
+    }
+
 }
--- test/MyClassTest.php
+++ test/MyClassTest.php
@@ -7,4 +7,9 @@ class MyClassTest extends PHPUnit_Framework_TestCase {
     $this->assertNotNull($c);
   }
 
+  public function testAdder() {
+    $c = new MyClass();
+    $this->assertEquals(5, $c->adder(2,3));
+  }
+
 }
